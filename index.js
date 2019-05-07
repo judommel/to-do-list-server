d94e44e4add2f0aa7c9583baceb06711005b0644
Julien <j_dommel@yahoo.fr>
2019-05-07 14:35:02 +0200

    basic routes made

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/to-do-list",
+  {
+    useNewUrlParser: true
+  }
+);
 
 const ToDo = mongoose.model("ToDo", { title: String, done: Boolean });
 
 app.get("/", async (req, res) => {
   try {
     const todos = await ToDo.find();
+    await todos.save();
 
-    return res.json(ToDo);
+    return res.json(todos);
   } catch (error) {
     console.log(error);
     res.status(400).json("Couldn't find your to do list.");
@@ -32,15 +36,25 @@ app.post("/create", async (req, res) => {
     });
 
     await toDo.save();
-    return res.json(`You know have "${req.body.title}" to do!`);
+    return res.json(`You now have "${req.body.title}" to do!`);
   } catch (error) {
     console.log(error);
     res.status(400).json("Error");
   }
 });
 
-app.post("/update", (req, res) => {
+app.post("/update", async (req, res) => {
   try {
+    const toDo = await ToDo.findById(req.body.id);
+
+    if (toDo.done) {
+      toDo.done = false;
+    } else {
+      toDo.done = true;
+    }
+
+    await toDo.save();
+
     return res.json("Updated!");
   } catch (error) {
     console.log(error);
@@ -48,9 +62,13 @@ app.post("/update", (req, res) => {
   }
 });
 
-app.post("/delete", (req, res) => {
+app.post("/delete", async (req, res) => {
   try {
-    return res.json("Deleted!");
+    const toDo = await ToDo.findById(req.body.id);
+
+    await toDo.remove();
+
+    return res.json(`Deleted! You don't have to do "${toDo.title}" anymore`);
   } catch (error) {
     console.log(error);
     res.status(400).json("Error");
